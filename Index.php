6aca7012638c1f83024effc06a023fec24a5a748
Gelser Cardona <gelsercardona@gmail.com>
2024-03-13 05:25:57 +0000

    test

---
+++ Index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actividad 3</title>
+<body>
+   
+</body>
+</html>
+<?php
+ echo "<h1 align= center>Hola Mundo, mi nombre es Gelser Cardona y me gusta programar😎 
+ <br> y las Motos.</h1>"
+?>
+<img src="Moto.jpg" alt="Moto">
